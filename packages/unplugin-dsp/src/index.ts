901d8195482c781e4d4e528ba837fa21c1c6f4b8
Michael Ostermann <michaelostermann@me.com>
2025-12-06 14:34:29 +0100

    chore: migrations

--- packages/unplugin-dsp/src/index.ts
+++ packages/unplugin-dsp/src/index.ts
@@ -25,7 +25,7 @@ export default createUnplugin<Options>(({ debug, enforce, exclude, include } = {
                     debug: shouldDebug(id),
                     resolve({ importAlias, importName, importPath, propertyName }) {
                         if (importPath === "@monstermann/dsp" && importName === "Dsp")
-                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/dsp/Dsp/${propertyName}.js"`
+                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/dsp/Dsp/${propertyName}.mjs"`
                         return undefined
                     },
                 })
